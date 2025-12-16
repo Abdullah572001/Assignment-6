910509666639f75d505bbbef1cffd1d14a79b08a
mohammadalamin600 <68004229+mohammadalamin600@users.noreply.github.com>
2025-12-16 20:09:21 +0600

    addad the radmi file

--- js/script.js
+++ js/script.js
@@ -33,7 +33,12 @@ const showPets = (pets) => {
   petContainer.innerHTML = "";
 
   if (pets.length === 0) {
-    petContainer.classList.add("bg-[#D2DCFD]", "border", "rounded-md", "border-none")
+    petContainer.classList.add(
+      "bg-[#D2DCFD]",
+      "border",
+      "rounded-md",
+      "border-none"
+    );
     petContainer.innerHTML = `
             <div></div>
             <div class="py-24">
@@ -58,38 +63,42 @@ const showPets = (pets) => {
                 <div class="flex gap-2 items-center">
                     <img class="w-4 h-4" src="https://img.icons8.com/?size=80&id=i83wF07inDck&format=png" />
                     <p class="text-gray-400">
-                        Breed : ${pet.breed}
+                        Breed : ${pet?.breed ? pet.breed : "Not found"}
                     </p>
                 </div>
 
                 <div class="flex gap-2 items-center">
                     <img class="w-4 h-4" src="https://img.icons8.com/?size=80&id=BCKAW92AuElD&format=png" />
                     <p class="text-gray-400">
-                        Birth : ${pet.date_of_birth}
+                        Birth : ${
+                          pet?.date_of_birth ? pet.date_of_birth : "Not found"
+                        }
                     </p>
                 </div>
 
                 <div class="flex gap-2 items-center">
                     <img class="w-4 h-4" src="https://ouch-prod-var-cdn.icons8.com/hm/illustrations/thumbs/Qj8QBJD5dvULD0CM.webp" />
                     <p class="text-gray-400">
-                        Gender : ${pet.gender}
+                        Gender : ${pet?.gender ? pet.gender : "Not Found"}
                     </p>
                 </div>
 
                 <div class="flex gap-2 items-center">
                     <img class="w-4 h-4" src="https://img.icons8.com/?size=50&id=7165&format=png" />
                     <p class="text-gray-400">
-                        Price : ${pet.price}
+                        Price : ${pet?.price ? pet.price : "Not found"}
                     </p>
                 </div>
 
                 <div class="flex justify-between items-center mt-6">
 
-                    <button class="btn text-[#0E7A81] hover:bg-[#0E7A81] hover:text-white font-bold"><img src="https://img.icons8.com/?size=24&id=82788&format=png" /></button>
+                    <button onclick="likeBtn('${pet.image}')" class="btn like-btn text-[#0E7A81] hover:bg-[#0E7A81] hover:text-white font-bold"><img src="https://img.icons8.com/?size=24&id=82788&format=png" /></button>
 
                     <button class="btn text-[#0E7A81] hover:bg-[#0E7A81] hover:text-white font-bold">Adopt</button>
 
-                    <button class="btn text-[#0E7A81] hover:bg-[#0E7A81] hover:text-white font-bold">Details</button>
+                    <button onclick="loadDetails('${
+                      pet.petId
+                    }')" class="btn text-[#0E7A81] hover:bg-[#0E7A81] hover:text-white font-bold">Details</button>
 
                 </div>
 
@@ -114,7 +123,7 @@ const loadByCategory = (categoryName) => {
 // click category button
 const categoryButton = (categoryName) => {
   // console.log(categoryName)
-  addBtnClass(categoryName)
+  addBtnClass(categoryName);
 
   const petContainer = document.getElementById("pet-section");
   petContainer.innerHTML = "";
@@ -128,13 +137,136 @@ const categoryButton = (categoryName) => {
 };
 
 // add and remove btn-style class
-const addBtnClass = name =>{
+const addBtnClass = (name) => {
   // console.log(name);
-  
-  const categoryBtn = document.getElementsByClassName('category-btn');
+
+  const categoryBtn = document.getElementsByClassName("category-btn");
   for (const btn of categoryBtn) {
-    btn.classList.remove('btn-style')
+    btn.classList.remove("btn-style");
   }
 
-  document.getElementById(name).classList.add("btn-style")
+  document.getElementById(name).classList.add("btn-style");
+};
+
+// load details by petid
+const loadDetails = async (petId) => {
+  // console.log(petId);
+
+  try {
+    const res = await fetch(
+      `https://openapi.programming-hero.com/api/peddy/pet/${petId}`
+    );
+    const data = await res.json();
+    showPetDetails(data.petData);
+
+    my_modal_1.showModal();
+  } catch (err) {
+    console.log("data not found", err);
+  }
+};
+
+// show pet details
+const showPetDetails = (petData) => {
+  const modalContainer = document.getElementById("modal-container");
+
+  modalContainer.innerHTML = `
+      <dialog id="my_modal_1" class="modal">
+        <div class="modal-box p-8">
+
+          <img class="w-full object-cover rounded-md" src="${petData.image}" />
+          <h2 class="text-2xl font-bold my-5">${petData.pet_name}</h2>
+          <div class="grid grid-cols-2 gap-2 pb-4 border-b border-b-gray-300">
+
+                <div class="flex gap-2 items-center">
+                    <img class="w-4 h-4" src="https://img.icons8.com/?size=80&id=i83wF07inDck&format=png" />
+                    <p class="text-gray-400">
+                        Breed : ${petData?.breed ? petData.breed : "Not found"}
+                    </p>
+                </div>
+
+                <div class="flex gap-2 items-center">
+                    <img class="w-4 h-4" src="https://img.icons8.com/?size=80&id=BCKAW92AuElD&format=png" />
+                    <p class="text-gray-400">
+                        Birth : ${
+                          petData?.date_of_birth
+                            ? petData.date_of_birth
+                            : "Not found"
+                        }
+                    </p>
+                </div>
+
+                <div class="flex gap-2 items-center">
+                    <img class="w-4 h-4" src="https://ouch-prod-var-cdn.icons8.com/hm/illustrations/thumbs/Qj8QBJD5dvULD0CM.webp" />
+                    <p class="text-gray-400">
+                        Gender : ${
+                          petData?.gender ? petData.gender : "Not found"
+                        }
+                    </p>
+                </div>
+
+                <div class="flex gap-2 items-center">
+                    <img class="w-4 h-4" src="https://img.icons8.com/?size=50&id=7165&format=png" />
+                    <p class="text-gray-400">
+                        Price : ${petData?.price ? petData.price : "not found"}
+                    </p>
+                </div>
+
+                <div class="flex gap-2 items-center">
+                    <img class="w-4 h-4" src="https://ouch-prod-var-cdn.icons8.com/hm/illustrations/thumbs/Qj8QBJD5dvULD0CM.webp" />
+                    <p class="text-gray-400">
+                        Vaccinated status : ${
+                          petData?.vaccinated_status
+                            ? petData.vaccinated_status
+                            : "Not found"
+                        }
+                    </p>
+                </div>
+
+          </div>
+
+          <h2 class="text-lg font-bold my-3">Details Information</h2>
+          <p class="text-xs text-gray-400">${
+            petData?.pet_details ? petData.pet_details : "Not found"
+          }</p>
+
+          <div class="modal-action w-full">
+            <form class="w-full" method="dialog">
+              <!-- if there is a button in form, it will close the modal -->
+              <button class="btn w-full bg-light-blue text-[#0E7A81]">Cancel</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+    `;
+};
+
+// like button function
+const likeBtn = (image) => {
+  // console.log(image);
+
+  const likeContainer = document.getElementById('like-container');
+
+  likeContainer.innerHTML += `
+    <div class="h-[124px] w-[124px] p-3 border border-gray-300">
+      <img class="h-full rounded-md object-cover" src="${image}" />
+    </div>
+  `;
+};
+
+
+// sort by price function
+const loadSortByPrice = () => {
+  // console.log('ht')
+
+  fetch('https://openapi.programming-hero.com/api/peddy/pets')
+    .then(res => res.json())
+    .then(data => showSortByPrice(data.pets))
+}
+
+const showSortByPrice = pets => {
+  console.log(pets);
+  
+  pets.sort((a, b) => b.price - a.price);
+
+  showPets(pets)
 }
